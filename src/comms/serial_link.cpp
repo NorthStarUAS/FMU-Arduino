460e288763c3d2b2280e6158afe5cbf50d547445
Curtis L. Olson <curtolson@flightgear.org>
2024-06-16 20:31:18 -0500

    update to 2-byte length format and tweaks to checksum function.

--- src/comms/serial_link.cpp
+++ src/comms/serial_link.cpp
@@ -6,18 +6,20 @@ SerialLink::SerialLink() {
 SerialLink::~SerialLink() {
 }
 
-void SerialLink::checksum( uint8_t hdr1, uint8_t hdr2, uint8_t *buf, uint8_t size, uint8_t *cksum0, uint8_t *cksum1 )
-{
+void SerialLink::checksum( uint8_t id, uint8_t len_lo, uint8_t len_hi, uint8_t *buf, uint16_t buf_size, uint8_t *cksum0, uint8_t *cksum1 ) {
     uint8_t c0 = 0;
     uint8_t c1 = 0;
 
-    c0 += hdr1;
+    c0 += id;
     c1 += c0;
 
-    c0 += hdr2;
+    c0 += len_lo;
     c1 += c0;
 
-    for ( uint8_t i = 0; i < size; i++ ) {
+    c0 += len_hi;
+    c1 += c0;
+
+    for ( uint16_t i = 0; i < buf_size; i++ ) {
         c0 += (uint8_t)buf[i];
         c1 += c0;
     }
@@ -25,6 +27,26 @@ void SerialLink::checksum( uint8_t hdr1, uint8_t hdr2, uint8_t *buf, uint8_t siz
     *cksum0 = c0;
     *cksum1 = c1;
 }
+
+// void SerialLink::checksum( uint8_t hdr1, uint8_t hdr2, uint8_t *buf, uint8_t size, uint8_t *cksum0, uint8_t *cksum1 )
+// {
+//     uint8_t c0 = 0;
+//     uint8_t c1 = 0;
+
+//     c0 += hdr1;
+//     c1 += c0;
+
+//     c0 += hdr2;
+//     c1 += c0;
+
+//     for ( uint8_t i = 0; i < size; i++ ) {
+//         c0 += (uint8_t)buf[i];
+//         c1 += c0;
+//     }
+
+//     *cksum0 = c0;
+//     *cksum1 = c1;
+// }
 
 bool SerialLink::open( int baud, int port ) {
     if ( port == 0 ) {
@@ -51,22 +73,22 @@ bool SerialLink::update() {
     // 0 = looking for SOM0
     // 1 = looking for SOM1
     // 2 = looking for packet id
-    // 3 = looking for packet len
+    // 3 = looking for packet len (note 2 bytes)
     // 4 = looking for packet data
     // 5 = looking for checksum_lo
     // 6 = looking for checksum_l=hi
     uint8_t input;
     bool new_data = false;
-    // Serial.print("start read_commands(): "); Serial.println(state);
+    // console->print("start read_commands(): "); console->println(state);
 
     if ( state == 0 ) {
         counter = 0;
         while ( _port->available() >= 1 ) {
             // scan for start of message
             input = _port->read();
-            // Serial.println(input);
+            // printf("%02x ", input);
             if ( input == START_OF_MSG0 ) {
-                // Serial.println("start of msg0");
+                // console->println("start of msg0");
                 state = 1;
                 break;
             }
@@ -76,7 +98,7 @@ bool SerialLink::update() {
         if ( _port->available() >= 1 ) {
             input = _port->read();
             if ( input == START_OF_MSG1 ) {
-                // Serial.println("start of msg1");
+                // console->println("start of msg1");
                 state = 2;
             }
             else if ( input == START_OF_MSG0 ) {
@@ -90,15 +112,18 @@ bool SerialLink::update() {
     if ( state == 2 ) {
         if ( _port->available() >= 1 ) {
             pkt_id = _port->read();
-            // Serial.print("id="); Serial.println(message_id);
+            // console->print("id="); console->println(message_id);
             state = 3;
         }
     }
     if ( state == 3 ) {
-        if ( _port->available() >= 1 ) {
-            pkt_len = _port->read();
-            // Serial.print("size="); Serial.println(pkt_len);
-            if ( pkt_len > 200 ) {
+        if ( _port->available() >= 2 ) {
+            pkt_len_lo = _port->read();
+            pkt_len_hi = _port->read();
+            pkt_len = pkt_len_hi << 8 | pkt_len_lo;
+            // console->printf("size=%d\n", pkt_len);
+            if ( pkt_len > MAX_MESSAGE_LEN ) {
+                printf("nonsense packet size, skipping.\n");
                 // ignore nonsensical sizes
                 state = 0;
             }  else {
@@ -108,12 +133,8 @@ bool SerialLink::update() {
     }
     if ( state == 4 ) {
         while ( _port->available() >= 1 && counter < pkt_len ) {
-            if ( counter < MAX_MESSAGE_LEN ) {
-                payload[counter++] = _port->read();
-                // Serial.println(buf[i], DEC);
-            } else {
-                state = 0;
-            }
+            payload[counter++] = _port->read();
+            // printf(buf[i], DEC);
         }
         if ( counter >= pkt_len ) {
             state = 5;
@@ -128,17 +149,17 @@ bool SerialLink::update() {
     if ( state == 6 ) {
         if ( _port->available() >= 1 ) {
             cksum_hi = _port->read();
-            byte cksum0, cksum1;
-            checksum( pkt_id, pkt_len, payload, pkt_len, &cksum0, &cksum1 );
+            uint8_t cksum0, cksum1;
+            checksum( pkt_id, pkt_len_lo, pkt_len_hi, payload, pkt_len, &cksum0, &cksum1 );
             if ( cksum_lo == cksum0 && cksum_hi == cksum1 ) {
-                // Serial.println("passed check sum!");
-                // Serial.print("pkt_id = "); Serial.println(pkt_id);
-                // Serial.print("size="); Serial.println(pkt_len);
+                // console->println("passed check sum!");
+                // console->print("pkt_id = "); console->println(pkt_id);
+                // console->print("size="); console->println(pkt_len);
                 // parse_message_bin( pkt_id, payload, pkt_len );
                 new_data = true;
                 state = 0;
             } else {
-                Serial.print("failed check sum, id = "); Serial.print(pkt_id); Serial.print(" len = "); Serial.println(pkt_len);
+                printf("failed check sum id: %d len: %d\n", pkt_id, pkt_len);
                 // check sum failure
                 state = 0;
             }
@@ -147,6 +168,107 @@ bool SerialLink::update() {
 
     return new_data;
 }
+
+// bool SerialLink::update() {
+//     // 0 = looking for SOM0
+//     // 1 = looking for SOM1
+//     // 2 = looking for packet id
+//     // 3 = looking for packet len
+//     // 4 = looking for packet data
+//     // 5 = looking for checksum_lo
+//     // 6 = looking for checksum_l=hi
+//     uint8_t input;
+//     bool new_data = false;
+//     // Serial.print("start read_commands(): "); Serial.println(state);
+
+//     if ( state == 0 ) {
+//         counter = 0;
+//         while ( _port->available() >= 1 ) {
+//             // scan for start of message
+//             input = _port->read();
+//             // Serial.println(input);
+//             if ( input == START_OF_MSG0 ) {
+//                 // Serial.println("start of msg0");
+//                 state = 1;
+//                 break;
+//             }
+//         }
+//     }
+//     if ( state == 1 ) {
+//         if ( _port->available() >= 1 ) {
+//             input = _port->read();
+//             if ( input == START_OF_MSG1 ) {
+//                 // Serial.println("start of msg1");
+//                 state = 2;
+//             }
+//             else if ( input == START_OF_MSG0 ) {
+//                 // no change
+//             } else {
+//                 // oops
+//                 state = 0;
+//             }
+//         }
+//     }
+//     if ( state == 2 ) {
+//         if ( _port->available() >= 1 ) {
+//             pkt_id = _port->read();
+//             // Serial.print("id="); Serial.println(message_id);
+//             state = 3;
+//         }
+//     }
+//     if ( state == 3 ) {
+//         if ( _port->available() >= 1 ) {
+//             pkt_len = _port->read();
+//             // Serial.print("size="); Serial.println(pkt_len);
+//             if ( pkt_len > 200 ) {
+//                 // ignore nonsensical sizes
+//                 state = 0;
+//             }  else {
+//                 state = 4;
+//             }
+//         }
+//     }
+//     if ( state == 4 ) {
+//         while ( _port->available() >= 1 && counter < pkt_len ) {
+//             if ( counter < MAX_MESSAGE_LEN ) {
+//                 payload[counter++] = _port->read();
+//                 // Serial.println(buf[i], DEC);
+//             } else {
+//                 state = 0;
+//             }
+//         }
+//         if ( counter >= pkt_len ) {
+//             state = 5;
+//         }
+//     }
+//     if ( state == 5 ) {
+//         if ( _port->available() >= 1 ) {
+//             cksum_lo = _port->read();
+//             state = 6;
+//         }
+//     }
+//     if ( state == 6 ) {
+//         if ( _port->available() >= 1 ) {
+//             cksum_hi = _port->read();
+//             byte cksum0, cksum1;
+//             checksum( pkt_id, pkt_len, payload, pkt_len, &cksum0, &cksum1 );
+//             if ( cksum_lo == cksum0 && cksum_hi == cksum1 ) {
+//                 // Serial.println("passed check sum!");
+//                 // Serial.print("pkt_id = "); Serial.println(pkt_id);
+//                 // Serial.print("size="); Serial.println(pkt_len);
+//                 // parse_message_bin( pkt_id, payload, pkt_len );
+//                 new_data = true;
+//                 state = 0;
+//             } else {
+//                 Serial.print("failed check sum, id = "); Serial.print(pkt_id); Serial.print(" len = "); Serial.println(pkt_len);
+//                 // check sum failure
+//                 state = 0;
+//             }
+//         }
+//     }
+
+//     return new_data;
+// }
 
 int SerialLink::bytes_available() {
     return _port->available();
@@ -160,19 +282,22 @@ int SerialLink::write_packet(uint8_t packet_id, uint8_t *payload, uint8_t len) {
     // packet id (1 byte)
     _port->write(packet_id);
 
-    // packet length (1 byte)
-    _port->write(len);
+    // packet length (2 bytes)
+    uint8_t len_lo = len & 0xFF;
+    uint8_t len_hi = len >> 8;
+    _port->write(len_lo);
+    _port->write(len_hi);
 
     // write payload
     _port->write( payload, len );
 
     // check sum (2 bytes)
     uint8_t cksum0, cksum1;
-    checksum( packet_id, len, payload, len, &cksum0, &cksum1 );
+    checksum( packet_id, len_lo, len_hi, payload, len, &cksum0, &cksum1 );
     _port->write(cksum0);
     _port->write(cksum1);
 
-    return len + 6;
+    return len + 7;
 }
 
 bool SerialLink::close() {
--- src/comms/serial_link.h
+++ src/comms/serial_link.h
@@ -14,16 +14,20 @@ private:
     int counter = 0;
     uint8_t cksum_lo = 0, cksum_hi = 0;
 
-    static const uint16_t MAX_MESSAGE_LEN = 200;
+    static const uint16_t MAX_MESSAGE_LEN = 1024;
+
+    void checksum( uint8_t id, uint8_t len_lo, uint8_t len_hi, uint8_t *buf, uint16_t buf_size, uint8_t *cksum0, uint8_t *cksum1 );
 
 public:
 
     static const uint8_t START_OF_MSG0 = 147;
     static const uint8_t START_OF_MSG1 = 224;
 
-    int pkt_id = 0;
-    int pkt_len = 0;
-    uint8_t payload[MAX_MESSAGE_LEN];
+    uint8_t pkt_id = 0;
+    uint8_t pkt_len_lo = 0;
+    uint8_t pkt_len_hi = 0;
+    uint16_t pkt_len = 0;
+    uint8_t payload[MAX_MESSAGE_LEN+1];
 
     uint32_t parse_errors = 0;
 
@@ -35,5 +39,4 @@ public:
     int bytes_available();
     int write_packet(uint8_t packet_id, uint8_t *payload, uint8_t len);
     bool close();
-    void checksum( uint8_t hdr1, uint8_t hdr2, uint8_t *buf, uint8_t size, uint8_t *cksum0, uint8_t *cksum1 );
 };
