641baa8f2a41ab696bda67cf45dca5232ed1489b
Curtis L. Olson <curtolson@flightgear.org>
2024-06-19 21:30:07 -0500

    Add gps message

--- src/comms/message_link.cpp
+++ src/comms/message_link.cpp
@@ -199,12 +199,20 @@ bool message_link_t::parse_message( uint8_t id, uint8_t *buf, uint8_t message_si
             }
             result = true;
         }
+    } else if ( id == ns_message::gps_v5_id ) {
+        ns_message::gps_v5_t msg;
+        msg.unpack(buf, message_size);
+        msg.msg2props(gps_node);
+        gps_node.setBool("settle", true);
+        gps_node.setDouble("latitude_deg", (double)(gps_node.getInt("latitude_raw")) / 10000000.0l);
+        gps_node.setDouble("longitude_deg", (double)(gps_node.getInt("longitude_raw")) / 10000000.0l);
+        gps_node.pretty_print();
     } else if ( id == ns_message::imu_v6_id ) {
         ns_message::imu_v6_t msg;
         msg.unpack(buf, message_size);
         msg.msg2props(imu_node);
         imu_node.setUInt("gyros_calibrated", 2);  // flag gyros from external source as calibrated
-        imu_node.pretty_print();
+        // imu_node.pretty_print();
     } else {
         printf("unknown message id: %d len: %d\n", id, message_size);
     }
