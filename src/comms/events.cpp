a2d937302b3f85911bb0fb9332e7f742811e8dc5
Curtis L. Olson <curtolson@flightgear.org>
2024-07-28 20:05:42 -0500

    Initial revision of a very simple events collector

---
+++ src/comms/events.cpp
@@ -0,0 +1,11 @@
+#include "events.h"
+
+void events_t::add_event(string header, string msg) {
+    event_list.push_back(header + ": " + msg);
+}
+
+void events_t::clear_events() {
+    event_list.clear();
+}
+
+events_t *events = NULL;
---
+++ src/comms/events.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+class events_t {
+
+public:
+
+    vector<string> event_list;
+    void add_event(string header, string msg);
+    void clear_events();
+
+};
+
+extern events_t *events;
