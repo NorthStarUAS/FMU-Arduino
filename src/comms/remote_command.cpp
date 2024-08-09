9c3936ccd4fa52815b8dfb060744810536a931ca
Curtis L. Olson <clolson@cirrusaircraft.com>
2024-08-09 07:18:06 -0500

    implement the remote property value set function

--- src/comms/remote_command.cpp
+++ src/comms/remote_command.cpp
@@ -1,10 +1,13 @@
 #include "../props2.h"
 #include "../nav/nav_mgr.h"
 #include "../sensors/sensor_mgr.h"
+#include "../util/strutils.h"
 #include "remote_command.h"
 
 void execute_command(string command, SerialLink *serial) {
     uint8_t result = 0;
+        vector<string> tokens = split(command, " ", 3);
+    // fixme: use tokens[0] rather than raw command string
     if ( command == "hb" ) {
         result = 1;
     } else if ( command == "zero_gyros" ) {
@@ -23,6 +26,61 @@ void execute_command(string command, SerialLink *serial) {
         reply.pack();
         serial->write_packet( reply.id, reply.payload, reply.len);
         result = 1;
+    } else if ( tokens[0] == "set" ) {
+        if ( tokens[1][0] == '/' ) {
+            // requires absolute path
+            printf("  %s\n", tokens[1].c_str());
+            size_t pos = tokens[1].rfind("/");
+            if ( pos != string::npos ) {
+                string node_path = tokens[1].substr(0, pos);
+                string name = tokens[1].substr(pos+1);
+                PropertyNode node( node_path );
+
+                // estimate value type (quick, not perfect!)
+                bool is_int = true;
+                bool is_float = false;
+                bool is_string = false;
+                for ( unsigned int i = 0; i < tokens[2].length(); i++ ){
+                    char c = tokens[2][i];
+                    if ( (c == '+' or c == '-') and i == 0 ) {
+                        // ok for the first character of int/float to be +/-
+                    } else if ( c >= '0' and c <='9' ) {
+                        // stick with the default (or current type estimate)
+                    } else if (c == '.') {
+                        if ( is_int ) {
+                            // we see a decimal point, promote to float if we currently think we have an int
+                            is_int = false;
+                            is_float = true;
+                        } else if ( is_float ) {
+                            // we already saw a '.' so another '.' is an invalid number, this must be a string!
+                            is_int = false;
+                            is_float = false;
+                            is_string = true;
+                        }
+                    } else {
+                        // non numeric characters, must be a string
+                        is_int = false;
+                        is_float = false;
+                        is_string = true;
+                    }
+                }
+                if ( is_int ) {
+                    node.setInt(name.c_str(), atoi(tokens[2].c_str()));
+                } else if ( is_float ) {
+                    node.setDouble(name.c_str(), atof(tokens[2].c_str()));
+                } else if ( is_string ) {
+                    if ( tokens[2] == "True" or tokens[2] == "true" ) {
+                        node.setBool(name.c_str(), true);
+                    } else if ( tokens[2] == "False" or tokens[2] == "false" ) {
+                        node.setBool(name.c_str(), false);
+                    } else {
+                        node.setString(name.c_str(), tokens[2]);
+                    }
+                }
+            } else {
+                // not a valid property name
+            }
+        }
     } else {
         // printf("unknown message: %s, relaying to host\n", command.c_str());
         // if ( relay_id == "gcs" ) {
