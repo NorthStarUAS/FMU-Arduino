988a5a21db9236a8ca997a8ee1c7f49cea72568f
Curtis L. Olson <curtolson@flightgear.org>
2024-08-17 19:34:00 -0500

    split the combined waypoint class into two separate classes: wp_rel_t and wp_abs_t.  The goal is more space efficiency on an embedded platform.  Previously this code ran on a beaglebone (linux, pi-like) and space efficiency was much less of a concern.

--- src/mission/waypoint.cpp
+++ src/mission/waypoint.cpp
@@ -2,30 +2,16 @@
 
 #include "waypoint.h"
 
-waypoint_t::waypoint_t( int mode, double coord1, double coord2 ) {
-    if ( mode == 0 ) {
-        absolute = false;
-        dist_m = coord1;
-        hdg_deg = coord2;
-    } else {
-        absolute = true;
-        lon_deg = coord1;
-        lat_deg = coord2;
-    }
-}
-
-void waypoint_t::build( PropertyNode config_waypoint_node ) {
+void wp_rel_t::build( PropertyNode config_waypoint_node ) {
     if ( config_waypoint_node.hasChild("lon_deg") and
          config_waypoint_node.hasChild("lat_deg") ) {
-        lon_deg = config_waypoint_node.getDouble("lon_deg");
-        lat_deg = config_waypoint_node.getDouble("lat_deg");
-        absolute = true;
-        printf("WPT: %.8f %.8f\n", lon_deg, lat_deg);
+        double lon_deg = config_waypoint_node.getDouble("lon_deg");
+        double lat_deg = config_waypoint_node.getDouble("lat_deg");
+        printf("Absolute wp in a relative route: %.8f %.8f\n", lon_deg, lat_deg);
     } else if ( config_waypoint_node.hasChild("heading_deg") and
                 config_waypoint_node.hasChild("dist_m") ) {
         hdg_deg = config_waypoint_node.getDouble("heading_deg");
         dist_m = config_waypoint_node.getDouble("dist_m");
-        absolute = false;
         printf("WPT: %4.0f deg %.0f m\n", hdg_deg, dist_m);
     } else {
         printf("Error in route waypoint config_waypoint_node logic:\n");
@@ -33,16 +19,32 @@ void waypoint_t::build( PropertyNode config_waypoint_node ) {
     }
 }
 
-void waypoint_t::update_relative_pos( double home_lon_deg, double home_lat_deg,
-                                      float ref_heading_deg ) {
-    if ( !absolute ) {
-        float course = ref_heading_deg + hdg_deg;
-        if ( course < 0.0 ) { course += 360.0; }
-        if ( course > 360.0 ) { course -= 360.0; }
-        double az2;
-        geo_direct_wgs_84( home_lat_deg, home_lon_deg, course, dist_m,
-                           &lat_deg, &lon_deg, &az2 );
+void wp_rel_t::update_relative_pos( double home_lon_deg, double home_lat_deg, float ref_heading_deg ) {
+    float course = ref_heading_deg + hdg_deg;
+    if ( course < 0.0 ) { course += 360.0; }
+    if ( course > 360.0 ) { course -= 360.0; }
+    double az2;
+    double lon_deg;
+    double lat_deg;
+    geo_direct_wgs_84( home_lat_deg, home_lon_deg, course, dist_m,
+                       &lat_deg, &lon_deg, &az2 );
+    longitude_raw = lon_deg * 10000000;
+    latitude_raw = lat_deg * 10000000;
+}
+
+void wp_abs_t::build( PropertyNode config_waypoint_node ) {
+    if ( config_waypoint_node.hasChild("lon_deg") and
+         config_waypoint_node.hasChild("lat_deg") ) {
+        longitude_raw = config_waypoint_node.getDouble("lon_deg") * 10000000;
+        latitude_raw = config_waypoint_node.getDouble("lat_deg") * 10000000;
+        printf("WPT: %lu %lu\n", longitude_raw, latitude_raw);
+    } else if ( config_waypoint_node.hasChild("heading_deg") and
+                config_waypoint_node.hasChild("dist_m") ) {
+        float hdg_deg = config_waypoint_node.getDouble("heading_deg");
+        float dist_m = config_waypoint_node.getDouble("dist_m");
+        printf("Relative waypoint in an absolute route: %4.0f deg %.0f m\n", hdg_deg, dist_m);
     } else {
-        printf("Error: cannot update relative position of absolute waypoint\n");
+        printf("Error in route waypoint config_waypoint_node logic:\n");
+        config_waypoint_node.pretty_print();
     }
 }
--- src/mission/waypoint.h
+++ src/mission/waypoint.h
@@ -1,18 +1,58 @@
 #include "../props2.h"
 
-class waypoint_t {
-
-public:
-    bool absolute = true;
+struct coord_t {
     double lon_deg = 0.0;
     double lat_deg = 0.0;
-    float hdg_deg = 0.0;
+};
+
+class wp_rel_t {
+
+public:
+
     float dist_m = 0.0;
+    float hdg_deg = 0.0;
+    uint32_t longitude_raw;
+    uint32_t latitude_raw;
     float leg_dist_m = 0.0;
 
-    waypoint_t() {}
-    waypoint_t( int mode, double coord1, double coord2 );
+    wp_rel_t() {}
+    wp_rel_t( float dist, float hdg ) {
+        dist_m = dist;
+        hdg_deg = hdg;
+    }
+    ~wp_rel_t() {}
+
     void build( PropertyNode config_waypoint_node );
     void update_relative_pos( double home_lon_deg, double home_lat_deg,
                               float ref_heading_deg );
+    coord_t as_coord() {
+        coord_t coord;
+        coord.lon_deg = longitude_raw / 10000000.0;
+        coord.lat_deg = latitude_raw / 10000000.0;
+        return coord;
+    }
+};
+
+class wp_abs_t {
+
+public:
+
+    uint32_t longitude_raw;
+    uint32_t latitude_raw;
+    float leg_dist_m = 0.0;
+
+    wp_abs_t() {}
+    wp_abs_t( uint32_t lon_raw, uint32_t lat_raw ) {
+        longitude_raw = lon_raw;
+        latitude_raw = lat_raw;
+    }
+    ~wp_abs_t() {}
+
+    void build( PropertyNode config_waypoint_node );
+    coord_t as_coord() {
+        coord_t coord;
+        coord.lon_deg = longitude_raw / 10000000.0;
+        coord.lat_deg = latitude_raw / 10000000.0;
+        return coord;
+    }
 };
