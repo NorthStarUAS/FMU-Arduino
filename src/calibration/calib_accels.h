04642d17776d25e670e63e9e69c4b8f1b1ef4107
Curtis L. Olson <curtolson@flightgear.org>
2024-10-04 04:55:37 -0500

    lowpass filter class name change

--- src/calibration/calib_accels.h
+++ src/calibration/calib_accels.h
@@ -12,12 +12,12 @@ private:
     uint32_t last_millis = 0;
     int state = -1;
     bool armed = false;
-    rcLowPassFilter ax_slow = rcLowPassFilter(1.0);
-    rcLowPassFilter ax_fast = rcLowPassFilter(0.2);
-    rcLowPassFilter ay_slow = rcLowPassFilter(1.0);
-    rcLowPassFilter ay_fast = rcLowPassFilter(0.2);
-    rcLowPassFilter az_slow = rcLowPassFilter(1.0);
-    rcLowPassFilter az_fast = rcLowPassFilter(0.2);
+    LowPassFilter ax_slow = LowPassFilter(1.0);
+    LowPassFilter ax_fast = LowPassFilter(0.2);
+    LowPassFilter ay_slow = LowPassFilter(1.0);
+    LowPassFilter ay_fast = LowPassFilter(0.2);
+    LowPassFilter az_slow = LowPassFilter(1.0);
+    LowPassFilter az_fast = LowPassFilter(0.2);
 
     Eigen:: MatrixXf Ref;
     Eigen::MatrixXf Meas;
