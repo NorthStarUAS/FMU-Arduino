c4623abf61a8753472504f5b8d8235a7485e3845
Curtis L. Olson <curtolson@flightgear.org>
2024-10-12 10:02:31 -0500

    depricate, but don't totally delete (yet) older sbus parsing code.

---
+++ src/sensors/sbus/sbus-aura.cpp
@@ -0,0 +1,178 @@
+#include "sbus-aura.h"
+
+// debug
+// #include "../../nodes.h"
+
+static const uint8_t SBUS_SIGNAL_OK = 0x00;
+static const uint8_t SBUS_SIGNAL_LOST = 0x01;
+static const uint8_t SBUS_SIGNAL_FAILSAFE = 0x03;
+
+static const uint8_t SBUS_HEADER_VALUE = 0x0F;
+static const uint8_t SBUS_FOOTER_VALUE = 0x00;
+
+static const int SBUS_MIN_VALUE = 172;
+static const int SBUS_CENTER_VALUE = 992;
+static const int SBUS_MAX_VALUE = 1811;
+static const int SBUS_RANGE = 1640;
+static const int SBUS_HALF_RANGE = 820;
+static const int SBUS_QUARTER_RANGE = 410;
+
+void sbus_aura_t::parse() {
+    // we don't need to return from these, these are just notifying us
+    // of receiver state
+    if ( sbus_data.failsafe_act ) {
+        // printf("SBUS: failsafe activated!\n");
+    }
+    if ( sbus_data.frame_lost ) {
+        // printf("SBUS: frame lost.\n");
+    }
+    raw_val[  0 ] = sbus_data.ch1;
+    raw_val[  1 ] = sbus_data.ch2;
+    raw_val[  2 ] = ( sbus_data.ch3_hi << 10 ) | sbus_data.ch3_lo;
+    raw_val[  3 ] = sbus_data.ch4;
+    raw_val[  4 ] = sbus_data.ch5;
+    raw_val[  5 ] = ( sbus_data.ch6_hi <<  9 ) | sbus_data.ch6_lo;
+    raw_val[  6 ] = sbus_data.ch7;
+    raw_val[  7 ] = sbus_data.ch8;
+    raw_val[  8 ] = ( sbus_data.ch9_hi <<  8 ) | sbus_data.ch9_lo;
+    raw_val[  9 ] = sbus_data.ch10;
+    raw_val[ 10 ] = sbus_data.ch11;
+    raw_val[ 11 ] = ( sbus_data.ch12_hi << 7 ) | sbus_data.ch12_lo;
+    raw_val[ 12 ] = sbus_data.ch13;
+    raw_val[ 13 ] = sbus_data.ch14;
+    raw_val[ 14 ] = ( sbus_data.ch15_hi << 6 ) | sbus_data.ch15_lo;
+    raw_val[ 15 ] = sbus_data.ch16;
+
+    uint8_t sbus_flags = 0x00;
+    sbus_flags |= sbus_data.ch17;
+    sbus_flags |= sbus_data.ch18 << 1;
+    sbus_flags |= sbus_data.frame_lost << 2;
+    sbus_flags |= sbus_data.failsafe_act << 3;
+
+    for ( int i = 0; i < SBUS_CHANNELS; i++ ) {
+        // convert to normalized form
+        if ( sbus_symmetrical[i] ) {
+            // i.e. aileron, rudder, elevator
+            norm_val[i] = (float)((int)raw_val[i] - SBUS_CENTER_VALUE) / SBUS_HALF_RANGE;
+            pwm_val[i] = (norm_val[i] + 1) * 500 + 1000;
+        } else {
+            // i.e. throttle, flaps
+            norm_val[i] = (float)((int)raw_val[i] - SBUS_MIN_VALUE) / SBUS_RANGE;
+            pwm_val[i] = norm_val[i] * 1000 + 1000;
+        }
+    }
+    Serial.print(millis()); Serial.print(" ail: "); Serial.print(pwm_val[3]); Serial.print(" "); Serial.println(norm_val[3], 3);
+    // inceptors_node.setUInt("debug", pwm_val[3]);
+
+#if 0
+    Serial.print(" ");
+    Serial.print(raw_val[0]);
+    Serial.print(" ");
+    Serial.print(raw_val[1]);
+    Serial.print(" ");
+    Serial.print(raw_val[2]);
+    Serial.print(" ");
+    Serial.print(raw_val[3]);
+    for ( int i = 0; i < SBUS_PAYLOAD_LEN; i++ ) {
+        Serial.print(" ");
+        Serial.print(sbus_data.buf[i], DEC);
+    }
+    Serial.println();
+#endif
+
+#if 0
+    for ( int i = 0; i < SBUS_CHANNELS; i++ ) {
+        if ( ch_data[i] < SBUS_MIN_VALUE || ch_data[i] > SBUS_MAX_VALUE ) {
+            Serial.print("Warning detected a problem with sbus packet data, skipping frame, ch = ");
+            Serial.println(i);
+            return;
+        }
+    }
+#endif
+
+    receiver_flags = sbus_flags;
+}
+
+// setup the sbus (currently hard coded on Serial2)
+void sbus_aura_t::init() {
+    Serial2.begin(100000,SERIAL_8E2_RXINV_TXINV); // newer teensies should use SERIAL_8E2_RXINV_TXINV
+    Serial.println("SBUS on Serial2 (SERIAL_8E2)");
+    // initialize to zero position
+    for ( int i = 0; i < SBUS_CHANNELS; i++ ) {
+        if ( sbus_symmetrical[i] ) {
+            // i.e. aileron, rudder, elevator
+            norm_val[i] = 0.0;
+            pwm_val[i] = (norm_val[i] + 1) * 500 + 1000;
+        } else {
+            // i.e. throttle, flaps
+            norm_val[i] = 0.0;
+            pwm_val[i] = norm_val[i] * 1000 + 1000;
+        }
+    }
+    // special case channels 0 (master autopilot enable) and 1 (throttle safety) to full off
+    norm_val[0] = -1; pwm_val[0] = 1000;
+    norm_val[1] = -1; pwm_val[1] = 1000;
+}
+
+// read available bytes on the sbus uart and return true if any new
+// data is read when a full packet is read, send that to the parser
+bool sbus_aura_t::process() {
+    static byte state = 0;
+    byte input;
+    bool new_data = false;
+
+    //Serial.print("state = ");
+    //Serial.println(state);
+    if ( state == 0 ) {
+        // scan for start of frame
+        while ( Serial2.available() > 0 ) {
+            new_data = true;
+            input = Serial2.read();
+            if ( input == SBUS_HEADER_VALUE ) {
+                state = 1;
+                break;
+            }
+        }
+    }
+    if ( state == 1 ) {
+        // fill in sbus frame (when enough bytes are available)
+        if ( Serial2.available() >= SBUS_PAYLOAD_LEN ) {
+            new_data = true;
+            for ( int i = 0; i < SBUS_PAYLOAD_LEN; i++ ) {
+                input = Serial2.read();
+                //Serial.print(" ");
+                //Serial.print(input, DEC);
+                sbus_data.buf[i] = input;
+            }
+            //Serial.println();
+            state = 2;
+        }
+    }
+    if  ( state == 2 ) {
+        //Serial.println("here in state = 2");
+        // end of frame
+        if ( Serial2.available() > 0 ) {
+            new_data = true;
+            //Serial.println("bytes are available");
+            input = Serial2.read();
+            if ( input == SBUS_FOOTER_VALUE ) {
+                parse();
+                state = 0;
+                // Serial.print("sbus:" ); Serial.println(millis());
+            } else {
+                //Serial.println("wrong sbus footer value, skipping ahead to next footer byte");
+                input = Serial2.read();
+                while ( Serial2.available() > 0 && input != SBUS_FOOTER_VALUE ) {
+                    input = Serial2.read();
+                }
+                if ( input == SBUS_FOOTER_VALUE ) {
+                    // set state to zero so we begin parsing the next new frame correctly
+                    // (but something went wrong with this frame so don't parse the data/discard.)
+                    state = 0;
+                }
+            }
+        }
+    }
+
+    return new_data;
+}
---
+++ src/sensors/sbus/sbus-aura.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <Arduino.h>
+
+const int SBUS_CHANNELS = 16;
+
+// define if an sbus input channel is symmetrical or not (i.e. mapped to
+// [0,1] for throttle, flaps, spoilers; [-1,1] for aileron, elevator, rudder
+static const bool sbus_symmetrical[SBUS_CHANNELS] = {1, 1, 0, 1, 1, 1, 1, 0, 0};
+
+class sbus_aura_t {
+
+private:
+
+    static const uint8_t SBUS_PAYLOAD_LEN = 23;
+    static const uint8_t SBUS_FRAMELOST = (1 << 2);
+    static const uint8_t SBUS_FAILSAFE = (1 << 3);
+
+    typedef union {
+        uint8_t buf[SBUS_PAYLOAD_LEN];
+
+        // Structure defining the contents of the SBUS data payload
+        // (23 bytes).  Each of the channel fields (ch1:ch16) occupies
+        // 11 bytes.
+        struct __attribute__ ((packed)) {
+            uint32_t ch1          : 11;
+            uint32_t ch2          : 11;
+            uint32_t ch3_lo       : 10;
+
+            uint32_t ch3_hi       :  1;
+            uint32_t ch4          : 11;
+            uint32_t ch5          : 11;
+            uint32_t ch6_lo       :  9;
+
+            uint32_t ch6_hi       :  2;
+            uint32_t ch7          : 11;
+            uint32_t ch8          : 11;
+            uint32_t ch9_lo       :  8;
+
+            uint32_t ch9_hi       :  3;
+            uint32_t ch10         : 11;
+            uint32_t ch11         : 11;
+            uint32_t ch12_lo      :  7;
+
+            uint32_t ch12_hi      :  4;
+            uint32_t ch13         : 11;
+            uint32_t ch14         : 11;
+            uint32_t ch15_lo      :  6;
+
+            uint32_t ch15_hi      :  5;
+            uint32_t ch16         : 11;
+            uint32_t ch17         :  1; // digital channel
+            uint32_t ch18         :  1; // digital channel
+            uint32_t frame_lost   :  1;
+            uint32_t failsafe_act :  1;
+        };
+    } SBUS_DATA_U;
+    SBUS_DATA_U sbus_data;
+    uint16_t raw_val[SBUS_CHANNELS];
+
+public:
+
+    uint8_t receiver_flags = 0x00;
+    void init();
+    void parse();
+    bool process();
+
+    float norm_val[SBUS_CHANNELS];    // normalized value (range of -1 to 1, or 0 to 1)
+    uint16_t pwm_val[SBUS_CHANNELS];  // equivalent-ish PWM value (range of 1000-2000)
+};
