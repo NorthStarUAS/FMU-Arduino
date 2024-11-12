8f21539c86ef60a7c1be446c97afc13604d96555
Curtis L. Olson <curtolson@flightgear.org>
2024-11-12 06:56:15 -0600

    We set airborne latch once we are aloft to enable throttle_safety on touchdown, but once we are landed, un-set the airborne latch so we can launch again.

--- src/mission/global/throttle_safety.cpp
+++ src/mission/global/throttle_safety.cpp
@@ -9,6 +9,7 @@ void throttle_safety_task_t::init() {
 void throttle_safety_task_t::update() {
     bool is_airborne = airdata_node.getBool("is_airborne");
     if ( not airborne_latch and is_airborne ) {
+        // aloft
         airborne_latch = true;
     }
 
@@ -16,7 +17,9 @@ void throttle_safety_task_t::update() {
         // if throttle is enabled, automatically disable if we have been
         // airborne, but now we are no longer flying (i.e. we landed)
         if ( airborne_latch and not is_airborne ) {
+            // touch down
             inceptors_node.setBool("throttle_enable", false);
+            airborne_latch = false;
             event_mgr->add_event("safety", "landed, throttle disabled!");
         }
     }
