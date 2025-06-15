aac94d75d10437d5d3256143babaa808338e3cff
Curtis L. Olson <curtolson@flightgear.org>
2025-06-15 11:25:27 -0500

    oops this add is now moved over to mixer.cpp as a last stage check to kill motor_enable, even if everything else is asking for motor_enable to be true.

--- src/mission/global/motor_safety.cpp
+++ src/mission/global/motor_safety.cpp
@@ -13,15 +13,6 @@ void motor_safety_task_t::update() {
         airborne_latch = true;
     }
 
-    if ( not home_node.getBool("calibrated") and not is_airborne ) {
-        // force motor_enable off (override operator switch) as long as home is
-        // not calibrated and we are on the ground.  note: a default home
-        // position is set when the system comes alive in case we somehow find
-        // ourselves in the air, but don't let a normal launch happen until home
-        // is properly calibrated.)
-        inceptors_node.setBool("motor_enable", false);
-    }
-
     if ( inceptors_node.getBool("motor_enable") ) {
         // if motor is enabled, automatically disable if we have been
         // airborne, but now we are no longer flying (i.e. we landed)
